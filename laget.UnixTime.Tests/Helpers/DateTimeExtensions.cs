3f3a1705750c99e744b7af8168e2dfaedefb5f50
Erik Hennerfors <erik.hennerfors@laget.se>
2023-03-24 15:40:46 +0100

    Added missing file

---
+++ laget.UnixTime.Tests/Helpers/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace laget.UnixTime.Tests.Helpers
+{
+    internal static class DateTimeExtensions
+    {
+        public static DateTime BeginningOfMinute(this DateTime dateTime)
+        {
+            return new DateTime(
+                dateTime.Year,
+                dateTime.Month,
+                dateTime.Day,
+                dateTime.Hour,
+                dateTime.Minute,
+                0,
+                0);
+        }
+    }
+}
